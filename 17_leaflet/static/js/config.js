f35f24bff73959c1a61c59be61e7082f5bc634b1
tbhaijee <61598731+tbhaijee@users.noreply.github.com>
2020-07-24 19:12:02 -0700

    add 17 leaflet hw

---
+++ 17_leaflet/static/js/config.js
@@ -0,0 +1,15 @@
+// API key
+var API_KEY = "pk.eyJ1IjoidGJoYWlqZWUiLCJhIjoiY2tjMjVydWZ1MTAxOTJ0b2U2YWszcHJ0OCJ9.YUFN3JxblWLs59aQ9-y3fQ";
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
